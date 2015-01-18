c4df14e15cb252b389f49621d5a3d79ebcd85f01
joeroza <abdul.rozak@live.com>
2015-01-18 23:17:09 +0700

    HRM.Spar

---
+++ HRM.Spar.Business/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRM.Spar.Entities;
+
+namespace HRM.Spar.Business
+{
+    public class Users
+    {
+        public sparEntities database = new sparEntities();
+        public static bool Login(string username, string password)
+        {
+      
+               
+        
+
+            
+            return true;
+        }
+
+        public static bool Register(string username, string password, string fullName, string email)
+        {
+            return true;
+        }
+    }
+}
---
+++ SparWeb/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HRM.Spar.Business;
+using HRM.Spar.Entities;
+using System.Web.Security;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (Users.Login(txtUsername.Text, txtPassword.Text))
+        {
+            FormsAuthentication.RedirectFromLoginPage
+             (txtUsername.Text, checkboxPersist.Checked);
+            //Response.Redirect("hrm");
+        }
+        else
+        { 
+            LabelError.Text = "Invalid credentials. Please try again.";
+        }
+    }
+}
